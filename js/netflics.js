07fde931eee446f43e32d7daec4eb3918f464e17
xtbl <correodecristobal@gmail.com>
2012-12-25 20:51:25 -0600

    base movie list

--- js/netflics.js
+++ js/netflics.js
@@ -1,6 +1,15 @@
 
 window.onload = function () {
-	
+		
+	//slider
+	/*$('.bxslider').bxSlider({
+		minSlides: 3,
+  		maxSlides: 4,
+  		slideWidth: 170,
+  		slideMargin: 10
+  	});
+	*/
+
 	//top level package
 	var NetFlics = NetFlics || {};
 
@@ -8,41 +17,54 @@ window.onload = function () {
 	NetFlics.getData = NetFlics.getData || {};
 
 	NetFlics.getData.utils = {
-		getMoviesByGenre: function(/*genre, quantity, orderBy*/ callback){
+		getMoviesByGenre: function(/*genre */ callback){
 			var api_key = '459887bb41a3478ab3139b788b2033e4';
 			var moviesByGenre;
 			$.ajax({
-			  //url: 'http://api.themoviedb.org/3/movie/550?api_key=459887bb41a3478ab3139b788b2033e4',
 			  url: 'http://api.themoviedb.org//3/genre/'+ 10749 +'/movies?api_key='+ api_key,
 			  dataType: 'jsonp',
 			  success: function(data) {
-			    moviesByGenre = data;
-			    alert('Load was performed.');
-			    alert('title: ' + moviesByGenre.results[0].title);
-			    //if(typeof callback === 'function'){ 
-			    	callback(moviesByGenre);
-			    	//alert('in callback');
-			    //}
-			  }//,
-			  //jsonp: 'jsonp'
+			    moviesByGenre = data.results;
+			    callback(moviesByGenre);
+			  }
 			});
-			//return moviesByGenre;
+		}
+	};
+
+	//defining package
+	NetFlics.dom = NetFlics.dom || {};
+
+	NetFlics.dom.utils = {
+		//receives movie list, cycle and adds 
+		addMoviePosters: function(movieList){
+			var movieLimit = 10;
+			var base_url = 'http://cf2.imgobject.com/t/p/w185';
+			//fadein for images 
+			$('#romance img').bind("load", function() {
+            	$('#romance').fadeIn();  
+          	});
+			if(movieList !== 'undefined'){
+				//add images and ids to each poster
+				for (var i = 0; i < movieLimit; i++) {
+					console.log('title in callback:' + movieList[i].title);
+					$('div#action ul li:eq('+ i +') img').attr('src', base_url + movieList[i].poster_path);
+					$('div#action ul li:eq('+ i +') a').attr('href', movieList[i].id);
+				};
+				console.log('title in callback in addMoviePosters:' + movieList[0].title);
+			}
 		}
 	};
 
 	$('#getmovie').click(function(e){
-		//var movieList;
 		e.preventDefault();
-		//using a callback to return data
+		//using a callback to get movie data
 		NetFlics.getData.utils.getMoviesByGenre(
-			function(movieList){
-				if(movieList !== 'undefined'){
-				alert('title in callback:' + movieList.results[0].title);
-				}
-			}
+			//add the movie posters after the ajax request has ended
+			NetFlics.dom.utils.addMoviePosters
 		);
-		//alert(movieList.results[0].title);
-		//$('#test-ajax').html(movieList);
+		$('#romance').elastislide();
 	});
-		
+
+
+  		
 }
